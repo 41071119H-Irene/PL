873aa641b4f197a313b2634094dd3b88c5e52687
41071119H-Irene <112916890+41071119H-Irene@users.noreply.github.com>
2022-10-22 23:07:43 +0800

    hw3 1/2

---
+++ HW3 python crawl/crawler.py
@@ -0,0 +1,16 @@
+# 抓取 PTT 新竹版的網頁原始碼(HTML)
+import urllib.request as req
+url= "https://www.ptt.cc/bbs/Hsinchu/index.html" # 可以換成想要抓的PTT版之網址
+# 建立一個 Request 物件，附加 Request Headers 的資訊(看起來像人類)
+request=req.Request(url, headers={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+})
+with req.urlopen(request) as response:
+    data=response.read().decode("utf-8")
+# 解析原始碼，取得每篇文章的標題
+import bs4
+root=bs4.BeautifulSoup(data, "html.parser")
+titles=root.find_all("div", class_="title") # 尋找所有 class="title" 的 div 標籤
+for title in titles:
+    if title.a !=None: # 如果標題包含 a 標籤(仍存在於網頁上)，就印出來
+        print(title.a.string)
